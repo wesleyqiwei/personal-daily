faeb5ce4b60e696662fb18fab93b9abd9f642ee0
wesley <wei.qi@stbchina.cn>
2022-07-27 17:20:35 +0800

    person map/curry

---
+++ imitate.js
@@ -0,0 +1,43 @@
+
+/**
+ * 
+ * @param {*} fn callback
+ * @returns 
+ */
+const PersonMap = function (fn) {
+    const mapRes = []
+    for (let i = 0;i<this.length; i++){
+        fn(this[i],i) && mapRes.push(fn(this[i],i))
+    }
+    return mapRes
+}
+
+Array.prototype.PersonMap = PersonMap
+
+console.log([1,2,3,4,5,6].PersonMap(item=>{
+    if(item>3){
+        return item
+    }
+}))
+
+/**
+ * 
+ * @param {*} fn 
+ * @returns 
+ */
+const PersonCurry = (fn) => {
+
+    return function curryHandle (...arg) {
+
+        if(fn.length > arg.length){
+            
+            return function(){
+                curryHandle(...[...arg,...arguments])
+            }
+        }
+
+        return fn(...arg)
+    }
+
+}
+
